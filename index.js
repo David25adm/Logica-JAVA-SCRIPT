b71c34fa7fba5baf42ee08c60e6643f8daec6b58
David25adm <165786937+David25adm@users.noreply.github.com>
2024-08-28 08:00:32 -0300

    JV SCRIPT

---
+++ index.js
@@ -0,0 +1,39 @@
+// Usando var
+var nome = "John"
+
+// Usando let
+let idade = 25;
+
+// Usando const
+const pi = 3.14;
+
+
+
+
+let a = 5;
+let b = 10;
+let soma = a + b ; // Armazena o resultado da soma de a e b na variavel sum console.log (soma) ; // 15
+
+
+
+let a = 5;
+let b = 10;
+
+function multiplica (x, y){
+    return x * y;
+}
+
+let produto = multiplica (a, b); // Armazena o resultado da funcao multiplica na varialvel produto
+console. log(produto); // 50
+
+let frutas = ['apple', 'banana', 'cherry'];
+console.log(frutas[1]); // banana
+
+
+
+let pessoa = {
+    primeiroNome: 'John',
+    sobrenome: 'Delon',
+    age: 25
+}
+
